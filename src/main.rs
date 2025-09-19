ad6008584f45c1d3160e1be03bbff347bf57ef72
dor cohen <dorcohen0203@gmail.com>
2025-09-19 23:21:49 +0300

    moved to axum

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use actix_files::Files;
-use actix_web::{App, HttpServer };
-// use sqlx::postgres::PgPoolOptions;
+use axum::Router;
+use tower_http::services::ServeDir;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenvy::dotenv().ok();
-    // let db_name = std::env::var("DBNAME").expect("no catalog provided");
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(10)
-    //     .connect(&format!("postgresql://localhost/{}", db_name))
-    //     .await
-    //     .unwrap();
+#[tokio::main]
+async fn main() {
+    let static_files = ServeDir::new("./target/dx/Ui/debug/web/public")
+    .append_index_html_on_directories(true);
 
-    HttpServer::new( || {
-        App::new()
-        .service(Files::new("/", "./target/dx/Ui/debug/web/public").index_file("index.html"))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
+    let app = Router::new().fallback_service(static_files);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
     .await
+    .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+// use sqlx::postgres::PgPoolOptions;
+// let db_name = std::env::var("DBNAME").expect("no catalog provided");
+// let pool = PgPoolOptions::new()
+//     .max_connections(10)
+//     .connect(&format!("postgresql://localhost/{}", db_name))
+//     .await
+//     .unwrap();
